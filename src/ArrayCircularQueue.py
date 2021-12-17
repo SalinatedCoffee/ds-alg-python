136b52a62a32ae5f3b6fa0c56d9f8ae51e28ba6e
SalinatedCoffee <juwon2015@hotmail.com>
2021-12-16 18:37:08 -0600

    Add front() and use separate constant for initial internal size

--- src/ArrayCircularQueue.py
+++ src/ArrayCircularQueue.py
@@ -1,10 +1,11 @@
-# TODO: rename to enqueue and dequeue, add method that returns first item without removing it
-# impliment dynamic size adjustment (factor of 2 for size increase, factor of .5 for decrease)
+# TODO: impliment dynamic size adjustment (factor of 2 for size increase, factor of .5 for decrease)
 # size should be decreased when elements are 1/4 of current capacity
 from src.PyDSError import *
 
 class ArrayCircularQueue():
-    def __init__(self, size=30):
+    DEFAULT_INTERNAL_SIZE = 10
+
+    def __init__(self, size=DEFAULT_INTERNAL_SIZE):
         self._data = [None] * size
         self._front = 0
         self._size = 0
@@ -12,6 +13,11 @@ class ArrayCircularQueue():
     def __len__(self):
         return self._size
     
+    def front(self):
+        if not self._size:
+            return None
+        return self._data[self._front]
+
     def enqueue(self, item):
         if len(self) == len(self._data):
             raise QueueOverCapacityException("unable to enqueue item to full queue")
