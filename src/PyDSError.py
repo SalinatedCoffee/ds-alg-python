ae9b000a11acb242f18c01de2e812f247fad7d1f
SalinatedCoffee <juwon2015@hotmail.com>
2021-12-30 13:02:47 -0600

    Add docstrings and housekeep for linter

--- src/PyDSError.py
+++ src/PyDSError.py
@@ -1,6 +1,9 @@
+"""Custom errors / exceptions for various data structures."""
+
 class QueueError():
+    """Custom errors / exceptions for ArrayCircularQueue and ArrayCircularDequeue."""
     class EmptyQueue(Exception):
-        pass
+        """Raised when attempting to dequeue item from already empty queue."""
 
     class MigrateSize(Exception):
-        pass
+        """Raised when internal resize target is smaller than number of elements in queue."""
