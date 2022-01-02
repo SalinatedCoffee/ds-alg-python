a846d079b0053c94d29857ddaed73e78830827be
SalinatedCoffee <juwon2015@hotmail.com>
2022-01-02 14:47:26 -0600

    Initial commit of tests for LinkedListQueue

---
+++ tests/test_LinkedListQueue.py
@@ -0,0 +1,27 @@
+import pytest
+from src.LinkedListQueue import LinkedListQueue
+from src.PyDSError import QueueError
+
+@pytest.fixture(name='empty_queue')
+def fixture_empty_queue():
+    """pytest fixture for empty linked list queue."""
+    return LinkedListQueue()
+
+def test_initialization(empty_queue):
+    """Test object initialization."""
+    with pytest.raises(QueueError.EmptyQueue):
+        empty_queue.dequeue()
+    assert empty_queue.front() is None
+    assert len(empty_queue) == 0
+
+def test_enqueue_dequeue(empty_queue):
+    """Test enqueue / dequeue of items."""
+    empty_queue.enqueue(3)
+    empty_queue.enqueue(5)
+    empty_queue.enqueue(7)
+    assert len(empty_queue) == 3
+    assert empty_queue.front() == 7
+    empty_queue.dequeue()
+    empty_queue.dequeue()
+    assert len(empty_queue) == 1
+    assert empty_queue.dequeue() == 3
