ff4d3f2ba934733619b55f7c2cd3f0c81d7a711b
SalinatedCoffee <juwon2015@hotmail.com>
2022-01-02 14:28:38 -0600

    Initial implementation

--- src/LinkedListStack.py
+++ src/LinkedListStack.py
@@ -2,10 +2,31 @@ from src.PyDSError import StackError
 
 class LinkedListStack():
     class _Node:
-        def __init__(self):
-            self._value = None
-            self._next = None
+        def __init__(self, value=None, next = None):
+            self._value = value
+            self._next = next
 
     def __init__(self):
-        self._head = self._Node()
-        self._size = 1
+        self._head = None
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+
+    def pop(self):
+        if not self._size:
+            raise StackError.EmptyStack("unable to pop item from empty stack")
+        temp = self._head
+        self._head = temp._next
+        self._size -= 1
+        return temp._value
+
+    def push(self, item):
+        temp = self._Node(item, self._head)
+        self._head = temp
+        self._size += 1
+
+    def top(self):
+        if not self._size:
+            return None
+        return self._head._value
