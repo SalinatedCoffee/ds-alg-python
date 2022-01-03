ac544ed87f1f264d27d030ef9803f0fcab483541
SalinatedCoffee <juwon2015@hotmail.com>
2022-01-03 14:53:40 -0600

    Fully implement stub methods and add docstrings

--- src/CircularLLQueue.py
+++ src/CircularLLQueue.py
@@ -1,7 +1,9 @@
 from src.PyDSError import QueueError
 
 class CircularLLQueue():
+    """Simple queue that supports right-to-left rotation operations."""
     class _Node:
+        """Internal class used for internal cirular linked list."""
         __slots__ = '_value', '_next'
         def __init__(self, value=None, next_ptr=None):
             self._value = value
@@ -15,13 +17,32 @@ class CircularLLQueue():
         return self._size
 
     def front(self):
-        return None
+        """Returns the value of the first node in the queue."""
+        if not self._size:
+            return None
+        return self._tail._next._value
 
     def enqueue(self, item):
-        return None
+        """Adds a node to the end of the queue with item as its value."""
+        if not self._size:
+            self._tail = self._Node(item)
+            self._tail._next = self._tail
+        else:
+            temp = self._Node(item, self._tail._next)
+            self._tail._next = temp
+            self._tail = temp
+        self._size += 1
 
     def dequeue(self):
-        return None
+        """Removes the first node in the queue and returns its value."""
+        if not self._size:
+            raise QueueError.EmptyQueue("unable to dequeue item from empty queue")
+        temp = self._tail._next
+        self._tail._next = temp._next
+        self._size -= 1
+        return temp._value
 
     def rotate(self):
-        return None
+        """Rotate the queue to the left by one item."""
+        if self._size:
+            self._tail = self._tail._next
