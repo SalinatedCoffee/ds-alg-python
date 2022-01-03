15c6cf9e74834f576097313187d91cda139cb6e9
SalinatedCoffee <juwon2015@hotmail.com>
2022-01-03 14:57:59 -0600

    Change docstrings to correctly reflect usage

--- src/PyDSError.py
+++ src/PyDSError.py
@@ -1,7 +1,7 @@
 """Custom errors / exceptions for various data structures."""
 
 class QueueError():
-    """Custom errors / exceptions for ArrayCircularQueue and ArrayCircularDequeue."""
+    """Custom errors / exceptions for queue objects."""
     class EmptyQueue(Exception):
         """Raised when attempting to dequeue item from already empty queue."""
 
@@ -9,6 +9,6 @@ class QueueError():
         """Raised when internal resize target is smaller than number of elements in queue."""
 
 class StackError():
-    """Custom errors / exceptions for LinkedListStack."""
+    """Custom errors / exceptions for stack objects."""
     class EmptyStack(Exception):
         """Raised when attempting to pop item from already empty queue."""
