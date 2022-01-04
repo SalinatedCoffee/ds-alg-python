c23e0b5188c71ff24610fc03b8eae73cbfdd0ad1
SalinatedCoffee <juwon2015@hotmail.com>
2022-01-04 16:48:31 -0600

    Implement tests for DoublyLinkedList

--- tests/test_DoublyLinkedList.py
+++ tests/test_DoublyLinkedList.py
@@ -1,7 +1,29 @@
 import pytest
 from src.DoublyLinkedList import DoublyLinkedList
 
-@pytest.fixture(name='basic_dll')
-def fixture_basic_dll():
-    temp = DoublyLinkedList()
-    return temp
+@pytest.fixture(name='empty_dll')
+def fixture_empty_dll():
+    """pytest fixture that instanciates a default doubly linked list."""
+    return DoublyLinkedList()
+
+def test_initialization(empty_dll):
+    """Test object initialization."""
+    assert len(empty_dll) == 0
+    assert empty_dll._header._value is None
+    assert empty_dll._trailer._value is None
+    assert empty_dll._header._next == empty_dll._trailer
+    assert empty_dll._trailer._prev == empty_dll._header
+
+def test_insert_remove(empty_dll):
+    """Test insert / remove methods."""
+    first = empty_dll.insert_between(1, empty_dll._header, empty_dll._trailer)
+    second = empty_dll.insert_between(3, first, empty_dll._trailer)
+    third = empty_dll.insert_between(5, second, empty_dll._trailer)
+    assert len(empty_dll) == 3
+    assert empty_dll.delete_node(second) == 3
+    assert len(empty_dll) == 2
+    assert empty_dll.delete_node(third) == 5
+    empty_dll.delete_node(first)
+    assert len(empty_dll) == 0
+    assert empty_dll._header._next == empty_dll._trailer
+    assert empty_dll._trailer._prev == empty_dll._header
