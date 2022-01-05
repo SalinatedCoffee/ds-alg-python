1c1182b85d1f136f61f4b101dcac1aed7022214e
SalinatedCoffee <juwon2015@hotmail.com>
2022-01-04 18:22:38 -0600

    Fully implement stubbed tests

--- tests/test_DoublyLinkedDeque.py
+++ tests/test_DoublyLinkedDeque.py
@@ -1,5 +1,6 @@
 import pytest
 from src.DoublyLinkedDeque import DoublyLinkedDeque
+from src.PyDSError import QueueError
 
 @pytest.fixture(name='empty_dld')
 def fixture_empty_dld():
@@ -8,8 +9,30 @@ def fixture_empty_dld():
 
 def test_initialization(empty_dld):
     """Test object initialization."""
-    assert False
+    assert len(empty_dld) == 0
+    with pytest.raises(QueueError.EmptyQueue):
+        empty_dld.delete_first()
+    with pytest.raises(QueueError.EmptyQueue):
+        empty_dld.delete_last()
+    assert empty_dld.front() is None
+    assert empty_dld.last() is None
 
 def test_enqueue_dequeue(empty_dld):
     """Test item enqueue / dequeue."""
-    assert False
+    empty_dld.insert_first(1)
+    empty_dld.insert_first(3)
+    empty_dld.insert_last(5)
+    empty_dld.insert_last(7)
+    assert len(empty_dld) == 4
+    assert empty_dld.front() == 3
+    assert empty_dld.last() == 7
+    assert empty_dld.delete_first() == 3
+    assert empty_dld.delete_last() == 7
+    assert len(empty_dld) == 2
+    assert empty_dld.front() == 1
+    assert empty_dld.last() == 5
+    empty_dld.delete_first()
+    assert empty_dld.front() == 5
+    assert empty_dld.last() == 5
+    empty_dld.delete_last()
+    assert len(empty_dld) == 0
