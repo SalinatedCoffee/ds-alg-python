cc39c797bf02e4f161e9ddf8cabf599f4d660f9c
SalinatedCoffee <juwon2015@hotmail.com>
2022-01-04 18:24:16 -0600

    Fix method calls

--- tests/test_DoublyLinkedDeque.py
+++ tests/test_DoublyLinkedDeque.py
@@ -14,7 +14,7 @@ def test_initialization(empty_dld):
         empty_dld.delete_first()
     with pytest.raises(QueueError.EmptyQueue):
         empty_dld.delete_last()
-    assert empty_dld.front() is None
+    assert empty_dld.first() is None
     assert empty_dld.last() is None
 
 def test_enqueue_dequeue(empty_dld):
@@ -24,15 +24,15 @@ def test_enqueue_dequeue(empty_dld):
     empty_dld.insert_last(5)
     empty_dld.insert_last(7)
     assert len(empty_dld) == 4
-    assert empty_dld.front() == 3
+    assert empty_dld.first() == 3
     assert empty_dld.last() == 7
     assert empty_dld.delete_first() == 3
     assert empty_dld.delete_last() == 7
     assert len(empty_dld) == 2
-    assert empty_dld.front() == 1
+    assert empty_dld.first() == 1
     assert empty_dld.last() == 5
     empty_dld.delete_first()
-    assert empty_dld.front() == 5
+    assert empty_dld.first() == 5
     assert empty_dld.last() == 5
     empty_dld.delete_last()
     assert len(empty_dld) == 0
