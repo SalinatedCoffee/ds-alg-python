1749fafbd8ca1a0edb29d2b160cd4bff223468ed
SalinatedCoffee <juwon2015@hotmail.com>
2022-01-04 18:24:54 -0600

    Commit initial implementation of DoublyLinkedDeque

--- src/DoublyLinkedDeque.py
+++ src/DoublyLinkedDeque.py
@@ -5,24 +5,32 @@ class DoublyLinkedDeque(DoublyLinkedList):
     """Simple deque using a doubly linked list as internal storage."""
     def first(self):
         """Returns the value of the first item in the deque."""
-        return None
+        if not self._size:
+            return None
+        return self._header._next._value
 
     def last(self):
         """Returns the value of the last item in the deque."""
-        return None
+        if not self._size:
+            return None
+        return self._trailer._prev._value
     
     def insert_first(self, item):
         """Inserts a new node at the front of the deque with item as its value."""
-        return None
+        self.insert_between(item, self._header, self._header._next)
 
     def insert_last(self, item):
         """Inserts a new node at the end of the deque with item as its value."""
-        return None
+        self.insert_between(item, self._trailer._prev, self._trailer)
 
     def delete_first(self):
         """Removes the node at the front of the deque and returns its value."""
-        return None
+        if not self._size:
+            raise QueueError.EmptyQueue("unable to dequeue item from empty queue")
+        return self.delete_node(self._header._next)
 
     def delete_last(self):
         """Removes the node at the end of the deque and returns its value."""
-        return None
+        if not self._size:
+            raise QueueError.EmptyQueue("umable to dequeue item from empty queue")
+        return self.delete_node(self._trailer._prev)
