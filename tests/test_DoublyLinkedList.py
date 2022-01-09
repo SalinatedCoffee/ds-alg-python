b8a61c332677d23e61f92b4a29466e6665147218
SalinatedCoffee <juwon2015@hotmail.com>
2022-01-08 22:38:32 -0600

    insert_between() and delete_node() methods are now private

--- tests/test_DoublyLinkedList.py
+++ tests/test_DoublyLinkedList.py
@@ -16,14 +16,14 @@ def test_initialization(empty_dll):
 
 def test_insert_remove(empty_dll):
     """Test insert / remove methods."""
-    first = empty_dll.insert_between(1, empty_dll._header, empty_dll._trailer)
-    second = empty_dll.insert_between(3, first, empty_dll._trailer)
-    third = empty_dll.insert_between(5, second, empty_dll._trailer)
+    first = empty_dll._insert_between(1, empty_dll._header, empty_dll._trailer)
+    second = empty_dll._insert_between(3, first, empty_dll._trailer)
+    third = empty_dll._insert_between(5, second, empty_dll._trailer)
     assert len(empty_dll) == 3
-    assert empty_dll.delete_node(second) == 3
+    assert empty_dll._delete_node(second) == 3
     assert len(empty_dll) == 2
-    assert empty_dll.delete_node(third) == 5
-    empty_dll.delete_node(first)
+    assert empty_dll._delete_node(third) == 5
+    empty_dll._delete_node(first)
     assert len(empty_dll) == 0
     assert empty_dll._header._next == empty_dll._trailer
     assert empty_dll._trailer._prev == empty_dll._header
