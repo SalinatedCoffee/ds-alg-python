c8d952b91864065b83d02138038a692d28c624cd
SalinatedCoffee <juwon2015@hotmail.com>
2022-01-13 16:52:54 -0600

    Implement object instantiation tests

--- tests/test_LinkedBinaryTree.py
+++ tests/test_LinkedBinaryTree.py
@@ -1,5 +1,10 @@
 import pytest
 from src.LinkedBinaryTree import LinkedBinaryTree
 
-def test_initialization():
-    assert False
+@pytest.fixture(name='empty_binary_tree')
+def fixture_empty_binary_tree():
+    return LinkedBinaryTree()
+
+def test_initialization(empty_binary_tree):
+    assert len(empty_binary_tree) == 0
+    assert empty_binary_tree.root() is None
