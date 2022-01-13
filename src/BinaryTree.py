1e14bc8eb69df7b57ea1bd7ebe41ab726d26534e
SalinatedCoffee <juwon2015@hotmail.com>
2022-01-12 20:34:24 -0600

    Implement sibling() and children()

--- src/BinaryTree.py
+++ src/BinaryTree.py
@@ -11,5 +11,19 @@ class BinaryTree(Tree):
         raise NotImplementedError()
 
     def sibling(self, p):
-        """Should return the sibling of node at position p."""
-        raise NotImplementedError()
+        """Returns the sibling of node at position p."""
+        parent = self.parent(p)
+        if not parent:
+            return None
+        else:
+            if p == self.left(parent):
+                return self.right(parent)
+            else:
+                return self.left(parent)
+    
+    def children(self, p):
+        """Generator that yields the children (in L->R order) of node at position p."""
+        if self.left(p):
+            yield self.left(p)
+        if self.right(p):
+            yield self.right(p)
