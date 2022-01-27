132ee2e40c24328b70c020ce8da5a50482435c69
SalinatedCoffee <juwon2015@hotmail.com>
2022-01-27 14:51:38 -0600

    Commit stub implementation with docstrings

---
+++ src/queue/unsorted_pqueue.py
@@ -0,0 +1,24 @@
+import src.queue.priority_queue as pqueue
+
+class unsorted_pqueue(pqueue):
+    def _find_min():
+        """Returns the Position of item with the smallest key."""
+        pass
+
+    def __init__():
+        pass
+
+    def __len__():
+        pass
+
+    def add():
+        """Adds a key-value pair to the queue."""
+        pass
+
+    def min():
+        """Returns, but does not remove, a tuple for the item with the smallest key."""
+        pass
+
+    def remove_min():
+        """Removes the item with the smallest key and returns its tuple representation."""
+        pass
