4d99a1fd9f9522abeefa606af13cfe3d433cea71
SalinatedCoffee <juwon2015@hotmail.com>
2022-01-29 19:13:47 -0600

    Commit initial passing implementation

--- src/queue/sorted_pqueue.py
+++ src/queue/sorted_pqueue.py
@@ -3,9 +3,13 @@ import src.list.positional_list as plist
 from src.exceptions.PyDSError import QueueError
 
 class sorted_pqueue(pqueue.priority_queue):
+    """Priority queue that uses a sorted linked list to store data."""
     def _find_min(self):
         """Returns a Position of the item with smallest key."""
-        pass
+        # return first item, since list is in ascending order
+        if not len(self):
+            raise QueueError.EmptyQueue('Queue is empty')
+        return self._data.first()
 
     def __init__(self):
         self._data = plist.positional_list()
@@ -15,12 +19,23 @@ class sorted_pqueue(pqueue.priority_queue):
 
     def add(self, k, v):
         """Adds a key-value pair to the queue."""
-        pass
+        item = self._Item(k, v)
+        current = self._data.last()
+        while current and current.element() > item:
+            current = self._data.before(current)
+        if not current:
+            self._data.add_first(item)
+        else:
+            self._data.add_after(current, item)
 
     def min(self):
         """Returns, but does not remove, a tuple for the item with the smallest key."""
-        pass
+        min = self._find_min().element()
+        return (min._key, min._value)
 
     def remove_min(self):
         """Removes the item with the smallest key and returns its tuple representation."""
-        pass
+        min = self._find_min()
+        temp = min.element()
+        self._data.delete(min)
+        return (temp._key, temp._value)
