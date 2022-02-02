d21d5a9f78e3330895d5ecb5fd1d1b8dccdefba8
SalinatedCoffee <juwon2015@hotmail.com>
2022-02-02 14:17:01 -0600

    Commit stub implementation with docstrings

---
+++ src/map/unsorted_map.py
@@ -0,0 +1,21 @@
+from src.map.map import map
+
+class unsorted_map(map):
+    """Simple table map that uses an unsorted list for internal storage."""
+    def __init__(self):
+        pass
+
+    def __getitem__(self, k):
+        pass
+
+    def __setitem__(self, k, v):
+        pass
+
+    def __delitem__(self, k):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __iter__(self):
+        pass
