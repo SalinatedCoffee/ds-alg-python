a42d00cc2b7bab8911a34cab9be1b6496bf4256b
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-02-02 15:27:20 -0600

    Commit initial passing implementation

--- src/map/unsorted_map.py
+++ src/map/unsorted_map.py
@@ -3,19 +3,31 @@ from src.map.map import map
 class unsorted_map(map):
     """Simple table map that uses an unsorted list for internal storage."""
     def __init__(self):
-        pass
+        self._table = []
 
     def __getitem__(self, k):
-        pass
+        for i in self._table:
+            if i._key == k:
+                return i._value
+        raise KeyError('Key error: ' + repr(k))
 
     def __setitem__(self, k, v):
-        pass
+        for i in self._table:
+            if i._key == k:
+                i._value = v
+                return
+        self._table.append(self._Item(k, v))
 
     def __delitem__(self, k):
-        pass
+        for n, i in enumerate(self._table):
+            if i._key == k:
+                self._table.pop(n)
+                return
+        raise KeyError('Key error: ' + repr(k))
 
     def __len__(self):
-        pass
+        return len(self._table)
 
     def __iter__(self):
-        pass
+        for i in self._table:
+            yield i._key
