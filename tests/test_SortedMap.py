3c11612aa0e41204d625e669d1345e62b24bf91d
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-02-06 20:55:28 -0600

    Commit stub implementation of unit tests for src.map.sorted_map

---
+++ tests/test_SortedMap.py
@@ -0,0 +1,33 @@
+from src.map.sorted_map import sorted_map
+
+def test_initialization():
+    """Tests object instantiation."""
+    assert False
+
+def test_add_items():
+    """Tests the addition of key-value pairs."""
+    assert False
+
+def test_mutate_items():
+    """Tests the modification of a value given its key."""
+    assert False
+
+def test_remove_items():
+    """Tests the removal of key-value pairs."""
+    assert False
+
+def test_ge_gt():
+    """Tests the behavior of 'greater-than' search methods."""
+    assert False
+
+def test_le_lt():
+    """Tests the behavior of 'less-than' search methods."""
+    assert False
+
+def test_iter():
+    """Tests the behavior of custom iterator methods."""
+    assert False
+
+def test_iter_range():
+    """Tests the behavior of sorted_map.find_range()."""
+    assert False
