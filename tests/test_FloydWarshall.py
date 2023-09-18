e70ef415c32fa3f3b88b2e03079060bcf143c39b
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-09-18 17:11:26 +0900

    Tidy up code

--- tests/test_FloydWarshall.py
+++ tests/test_FloydWarshall.py
@@ -1,13 +1,14 @@
 from src.algo.traverse import floydwarshall
 
 def test_correctness():
-  """Test against known graphs."""
-  # directed graph
-  adj = [[(2,-2)], [(0,4),(2,3)], [(3,2)], [(1,-1)]]
-  mat = floydwarshall(adj)
-  exp = [[0,-1,-2,0], [4,0,2,4], [5,1,0,2], [3,-1,1,0]]
-  assert len(mat) == len(exp)
-  assert len(mat[0]) == len(exp[0])
-  for i in range(len(exp)):
-    for j in range(len(exp[0])):
-      assert exp[i][j] == mat[i][j]
+    """Test against known graphs."""
+    # directed graph
+    adj = [[(2,-2)], [(0,4),(2,3)], [(3,2)], [(1,-1)]]
+    m = len(adj)
+    exp = [[0,-1,-2,0], [4,0,2,4], [5,1,0,2], [3,-1,1,0]]
+    mat = floydwarshall(adj)
+    assert len(mat) == len(exp)
+    assert len(mat[0]) == len(exp[0])
+    for i in range(m):
+        for j in range(m):
+            assert exp[i][j] == mat[i][j]
